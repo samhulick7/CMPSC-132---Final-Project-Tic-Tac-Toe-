ab089e8829b3f02bf87122bcc3168387ca753565
Samantha Hulick <samanthahulick@MacBook-Pro-2.local>
2026-04-28 21:40:42 -0400

    Main logic implemented

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# CMPSC-132---Final-Project-Tic-Tac-Toe-
-Tic-Tac-Toe Game
+# CMPSC-132: Final Project
+# Tic-Tac-Toe Game
+by Samantha Hulick
+
+Project Description:
+
+Two-player Tic-Tac-Toe game that uses a 3x3 playing board. The game utilizes the terminal to alternate turns between the players, 'X' and 'O', keeping track of the wins for each player.
+
+Instructions:
+
+1. Copy the file "Tic_Tac_Toe_Code.py"
+2. Open a terminal/command prompt. Upon first running the program and before each turn, the terminal will display the score, the board (represented as a list of lists), who's turn it is, as well as an input field for the row and column of the play.
+3. Respond to the command prompts that ask for the row and column index of the play. In order for a play to be marked on the board, the row and column of the play must be between 0-2, or else the play will be ruled invalid and the terminal will re-ask the player.
+4. If one player wins by connecting 3 in a row (either horizontally, vertically, or diagonally), the overall score will be updated. 
+5. Upon one player winning or a draw occuring, the terminal will prompt the player to type any variation of 'YES' if they want to play another game and further increase the score. After each game, the next starting player will be the loser of the previous game, or in the case of a draw it will be the opposite of whoever started the last game.
+6. However, if the player types anything other than yes to continuing the game, the game will end.
--- Tic_Tac_Toe_Code.py
+++ Tic_Tac_Toe_Code.py
@@ -1,6 +1,7 @@
 # CMPSC 132 Final Project:
 # Tic-Tac-Toe Game 
 # Created by Samantha Hulick
+# Github Username: samhulick7
 
 class TicTacToe():
      
@@ -8,26 +9,132 @@ class TicTacToe():
         self.board = [[' ',' ',' '],
                       [' ',' ',' '],
                       [' ',' ',' ']]
+        self.turn_counter = 0
+        self.x_win_count = 0
+        self.o_win_count = 0
         
 
     def print_board(self):
-        pass
+        
+        print(f"SCORE:\nX: {self.x_win_count} O: {self.o_win_count}")
+        print("--------------")
+        for row in self.board:
+            print(row)
+        print("--------------")
 
     def play(self):
-        pass
+        
+        game_end = False
+
+        while not game_end:
+
+            self.print_board()
+
+            valid_move = False
+
+            while not valid_move: # Loop continues until a valid input is given
+
+                try:
+
+                    if self.turn_counter % 2 == 0:
+                        player = "X"
+                        print("It's X's turn!")
+                    else:
+                        player = "O"
+                        print("It's O's turn!")
+
+                    row = int(input("Please enter the row (0-2): "))
+                    column = int(input("Please enter the column (0-2): "))
+
+                    if (row < 0) or(row > 2) or (column < 0) or (column > 2): # Input out of range, asks the player again
+                        self.print_board()
+                        print("Input must be between 0 and 2, please try again.")
+                    elif (self.board[row][column] != ' '): # Space already filled, asks the player again
+                        self.print_board()
+                        print("That space is already filled, please try again.")
+                    else:
+                        valid_move = True
+
+                except ValueError: # If input is anything else other than an integer
+                    self.print_board()
+                    print("Input must be an integer, please try again")
+            
+                
+            self.board[row][column] = player
+            self.turn_counter += 1
+
+            if self.check_winner(player):
+                
+                self.print_board()
+                
+                print(f"WINNER: {player}!") # Displays the winner and their rolling number of wins
+                if player == "X":
+                    print(f"{player}'s Number of Wins: {self.x_win_count}")
+                elif player == "O":
+                    print(f"{player}'s Number of Wins: {self.o_win_count}")
+
+                if not self.replay():
+                    game_end = True
+
+            elif self.is_draw():
+                
+                self.print_board()
+
+                print(f"DRAW!")
+
+                if not self.replay():
+                    game_end = True
+
+            # When there is no winner or no draw, the game continues like normal
+
     
     def check_winner(self, player):
-        pass
+        
+        for i in range(3):
+            if player == self.board[i][0] == self.board[i][1] == self.board[i][2]: # Horizontal win
+                self.increase_win_count(player)
+                return True
+            elif player == self.board[0][i] == self.board[1][i] == self.board[2][i]: # Vertical win
+                self.increase_win_count(player)
+                return True
+        if player == self.board[0][0] == self.board[1][1] == self.board[2][2]: # Diagonal win (top left to bottom right)
+            self.increase_win_count(player)
+            return True
+        elif player == self.board[0][2] == self.board[1][1] == self.board[2][0]: # Diagonal win (top right to bottom left)
+            self.increase_win_count(player)
+            return True 
+        
+        return False # No winner yet
+            
+    def increase_win_count(self, player):
+        
+        if player == "X":
+            self.x_win_count += 1
+        elif player == "O":
+            self.o_win_count += 1
 
 
     def is_draw(self):
-        pass
+        
+        return self.is_full()
 
     def is_full(self):
-        pass
+        
+        for row in self.board:
+            for item in row:
+                if item == ' ':
+                    return False
+        return True
     
     def replay(self):
-        pass
+        play_again = input("Type YES to play again!: ").upper()
+        
+        if play_again == "YES":
+            self.clear_board()
+            return True
+        else:
+            print("Thank you for playing!")
+            return False
 
 
     def clear_board(self):
@@ -35,3 +142,13 @@ class TicTacToe():
         self.board = [[' ',' ',' '],
                       [' ',' ',' '],
                       [' ',' ',' ']]
+        
+        # I chose not to reset self.turn_counter = 0 so that the loser of each game will start the next game
+        
+
+
+
+# Start of Game
+print("\nWelcome to Tic-Tac-Toe! \nType in your terminal to play:\n")
+game = TicTacToe()
+game.play()
