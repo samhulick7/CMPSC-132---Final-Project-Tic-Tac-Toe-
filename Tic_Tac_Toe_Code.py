c702619fcdb734e020f5dae0b28b523616c6aadc
Samantha Hulick <samanthahulick@MacBook-Pro-2.local>
2026-04-28 20:07:56 -0400

    Initializing functions

--- Tic_Tac_Toe_Code.py
+++ Tic_Tac_Toe_Code.py
@@ -1,3 +1,37 @@
 # CMPSC 132 Final Project:
 # Tic-Tac-Toe Game 
 # Created by Samantha Hulick
+
+class TicTacToe():
+     
+    def __init__(self):
+        self.board = [[' ',' ',' '],
+                      [' ',' ',' '],
+                      [' ',' ',' ']]
+        
+
+    def print_board(self):
+        pass
+
+    def play(self):
+        pass
+    
+    def check_winner(self, player):
+        pass
+
+
+    def is_draw(self):
+        pass
+
+    def is_full(self):
+        pass
+    
+    def replay(self):
+        pass
+
+
+    def clear_board(self):
+        
+        self.board = [[' ',' ',' '],
+                      [' ',' ',' '],
+                      [' ',' ',' ']]
